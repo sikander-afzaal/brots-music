287e39833090301d44891ce101f2f6c74e82c6a4
sikander-afzaal <sikander.aaa@gmail.com>
2023-01-15 11:30:07 +0500

    asd

---
+++ components/Landing/Project.jsx
@@ -0,0 +1,9 @@
+const Project = () => {
+  return (
+    <div className="wrapper mt-[100px]">
+      <div className="contain justify-center items-start  "></div>
+    </div>
+  );
+};
+
+export default Project;
