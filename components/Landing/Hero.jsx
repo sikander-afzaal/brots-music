1b388347c00d71140403c8d06c209c0c3f331652
sikander-afzaal <sikander.aaa@gmail.com>
2023-01-16 23:04:30 +0500

    asd

--- components/Landing/Hero.jsx
+++ components/Landing/Hero.jsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 const Hero = () => {
   return (
     <div
-      className="wrapper bg-none mid:bg-heroMid 3xl:min-h-[1304px] 2xl:min-h-[985px] 1xl:min-h-[675px]
-    xl:min-h-[675px] min-h-screen 3xl:bg-heroBig py-[100px] bg-contain bg-right-top bg-no-repeat "
+      className="flex justify-start items-start flex-col w-full bg-none mid:bg-heroMid 3xl:min-h-[1304px] 2xl:min-h-[985px] 1xl:min-h-[675px]
+    xl:min-h-[675px] min-h-screen 3xl:bg-heroBig py-[140px] bg-contain bg-right-top bg-no-repeat "
     >
-      <div className="flex w-full mid:gap-[100px] lg:gap-[150px] 3xl:gap-[60px] flex-col justify-center items-start h-full">
-        <h1 className="text-white uppercase mx-auto leading-[1] w-full max-w-[90%] font-bold text-[10vw]  mid:text-[7vw] 2xl:text-[145px] 3xl:text-[200px]">
+      <div className="flex w-full mid:gap-[100px] lg:gap-[150px] 3xl:gap-[90px] flex-col justify-center items-start h-full">
+        <h1 className="text-white uppercase mx-auto leading-[1] w-full max-w-[90%] font-bold text-[10vw]  mid:text-[7vw] 2xl:text-[145px] 3xl:text-[180px]">
           Music <br /> in <span className="animate-colorChange">Limited</span>{" "}
           <br /> Edition
         </h1>
